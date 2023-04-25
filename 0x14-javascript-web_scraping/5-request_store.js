2441b9afac43be275904fdfdf05c0fdad322645c
theodes1 <obasam67@gmail.com>
2023-04-25 18:28:04 +0100

    script that gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 // script that gets the contents of a webpage and stores it in a file.
-const axios = require('axios');
+const request = require('request');
 const fs = require('fs');
-const args = process.argv.slice(2);
 
-axios.get(args[0])
-  .then(response => {
-    fs.writeFile(process.argv[3], response.data, error => {
-      if (error) {
-        console.error(error);
-      }
-    });
-  });
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
