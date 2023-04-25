6d3bc24aabcef1c81fb0c12d446459eb41627e5a
theodes1 <obasam67@gmail.com>
2023-04-25 18:25:22 +0100

    script that prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
 // script that prints the title of a Star Wars movie
-const axios = require('axios');
-const args = process.argv.slice(2);
-let appearances = 0;
+const request = require('request');
+let nFilms = 0;
 
-axios.get(args[0])
-  .then(response => {
-    const films = response.data.results ? response.data.results : [];
-    const size = films.length;
-    for (let count = 0; count < size; count++) {
-      films[count].characters.forEach(character => {
-        if (character.includes('18')) {
-          appearances += 1;
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
+          nFilms++;
         }
-      });
+      }
     }
-    console.log(appearances);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+  }
+  console.log(nFilms);
+});
