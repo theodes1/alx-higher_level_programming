7442f7b9a562966a2901d6398fa20d2789c69ee4
theodes1 <obasam67@gmail.com>
2023-04-25 18:16:18 +0100

    script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 // script that display the status code of a GET request
-const axios = require('axios');
-const args = process.argv.slice(2);
-
-axios.get(args[0])
-  .then(response => {
-    console.log('code:', response.status);
-  })
-  .catch(error => {
-    console.log('code:', error.response.status);
-  });
+const request = require('request');
+request(process.argv[2], function (error, response) {
+  if (error == null) {
+    console.log('code: ' + response.statusCode);
+  }
+});
